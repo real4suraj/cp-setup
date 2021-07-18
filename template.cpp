58b73b82b42414a12264fa859f5de0ea1768902d
Suraj Patel <67200192+real4suraj@users.noreply.github.com>
2021-07-18 11:48:20 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,60 @@
+//+LNCT++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+---------------------Lord Escanor : Sin Of Pride-------------------------+//
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++BHOPAL+//
+
+#include <bits/stdc++.h>
+
+#define fo(i, n)            for(int i = 0; i < int(n); i++)
+#define of(i, n)            for(int i = int(n) - 1; i >= 0; i--)
+#define Fo(i, l, r)         for(int i = int(l); i < int(r); i++)
+#define oF(i, l, r)         for(int i = int(r) - 1; i >= int(l); i--)
+#define valid(x, y, n, m)   (0 <= (x) and (x) < (n) and 0 <= (y) and (y) < (m))
+#define all(a)              (a).begin(), (a).end()
+#define sz(a)               int((a).size())
+#define pb(a)               push_back(a)
+#define mp(x, y)            make_pair((x), (y))
+#define x                   first
+#define y                   second
+#define endl                '\n'
+#define deb(x)              cout << #x << " " << x << endl;
+#define fastIO              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+typedef long long                          ll;
+typedef long double                        ld;
+typedef pair<int, int>                     pii;
+typedef pair<long long, long long>         pll;
+typedef vector<bool>                       vb;
+typedef vector<int>                        vi;
+typedef vector<long long>                  vll;
+typedef vector<pair<int, int>>             vpii;
+typedef vector<pair<long long, long long>> vpll;
+typedef vector<vector<bool>>               vvb;
+typedef vector<vector<int>>                vvi;
+typedef vector<vector<long long>>          vvll;
+
+const int INF = int(1e9);
+const ll INF64 = ll(1e18);
+const ld EPS = 1e-9, PI = 3.1415926535897932384626433832795;
+
+void solve() {
+}
+
+int main() {
+    fastIO;
+//#ifdef LOCAL
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+//#endif
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+#ifdef LOCAL
+    cout<<"\nTime Elapsed: " << 1.0*clock() / CLOCKS_PER_SEC << " sec\n";
+#endif
+
+    return 0;
+}
